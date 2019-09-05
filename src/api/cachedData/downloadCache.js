31fd43576aff2ce87959ad9da334ffbf44a33e26
Alejandro Paredes Alva <a.alva@globalscanning.com>
2019-09-05 10:32:30 +0200

    dont ignore spread eslint

--- src/api/cachedData/downloadCache.js
+++ src/api/cachedData/downloadCache.js
@@ -259,8 +259,7 @@ function fetchFromUrl(url) {
 const types = ['books', 'characters', 'houses'];
 Promise.all(types.map(githubUrlForType).map(fetchFromUrl))
   .then(fixturesList => {
-    // eslint-disable-next-line prefer-spread
-    const fixtures = [].concat.apply([], fixturesList);
+    const fixtures = [].concat(...fixturesList);
     const objects = getObjectsFromFixtures(fixtures);
     populateReverseMaps(objects);
     const cache = getCacheFromObjects(objects);
